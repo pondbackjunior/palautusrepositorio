f421f325ef5eb934c7a2ee1893928ed7bffc0983
Ville Lampiselkä <148494501+pondbackjunior@users.noreply.github.com>
2024-11-11 00:04:05 +0200

    changed index

--- viikko2/index.py
+++ viikko2/index.py
@@ -1,8 +1,14 @@
+# tehdään alussa importit
+
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
